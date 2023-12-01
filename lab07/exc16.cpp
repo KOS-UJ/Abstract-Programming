6368cc39324b246d27fc90c827a3e6af885312ef
Piotr <prbartman@gmail.com>
2023-12-01 13:54:37 +0100

    lab07

--- lab07/exc16.cpp
+++ lab07/exc16.cpp
@@ -1,23 +1,24 @@
-    #include <iostream>
+#include <iostream>
 
-    /*
-     * Bazując na przykłądzie zaimplementować funkcję liczącą exp(x) z zdaną dokłądnością.
-     */
+/*
+ * Bazując na przykłądzie zaimplementować funkcję liczącą
+ * exp(x) z zdaną dokłądnością.
+ */
 
-    template<int N>
-    class Factorial {
-    public:
-        enum { value = N * Factorial<N-1>::value };
-    };
+template<int N>
+class Factorial {
+public:
+    enum { value = N * Factorial<N-1>::value };
+};
 
-    template<>
-    class Factorial<1> {
-    public:
-        enum { value = 1 };
-    };
+template<>
+class Factorial<1> {
+public:
+    enum { value = 1 };
+};
 
-    int main()
-    {
-        std::cout << Factorial<5>::value;
-        return 0;
-    }
+int main()
+{
+    std::cout << Factorial<5>::value;
+    return 0;
+}
