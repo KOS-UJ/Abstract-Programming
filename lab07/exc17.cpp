22dc9965e636285afc0a07cfcb11b4e1a13ae571
Piotr <prbartman@gmail.com>
2024-11-21 10:29:56 +0100

    update lab07

--- lab07/exc17.cpp
+++ lab07/exc17.cpp
@@ -1,4 +1,29 @@
+#include <iostream>
+#include <cassert>
+
 /***
-  * Korzystając z https://en.wikipedia.org/wiki/Exponentiation#Efficient_computation_with_integer_exponents
-  * zaimplementować efektywne liczenie potęgi z wykorzystaniem TMP.
- ***/
+ * "Scalić" klasy "ArrayFast" i "ArraySafe" w klasę "Array" tak aby:
+ * Array<int, 44> - bezpieczna tablica 44 intów
+ * Array<int, 44, AccessSafe> - bezpieczna tablica 44 intów
+ * Array<double, 100000, AccessFast> - szybka tablica 100tys. doubli
+ * Wykorzystaj klasy wytycznych (call policies).
+ */
+
+template<typename T, int size> class ArrayFast {
+public:
+    T& operator[](int index) {return tab[index];}
+private:
+    T tab[size];
+};
+
+template<typename T, int size> class ArraySafe {
+public:
+    T &operator[](int index) {
+        if (index < 0 || index > size)
+            throw std::out_of_range("ArraySave range");
+        return tab[index];
+    }
+
+private:
+    T tab[size];
+};
---
+++ lab08/exc17.cpp
@@ -0,0 +1,4 @@
+/***
+  * Korzystając z https://en.wikipedia.org/wiki/Exponentiation#Efficient_computation_with_integer_exponents
+  * zaimplementować efektywne liczenie potęgi z wykorzystaniem TMP.
+ ***/
