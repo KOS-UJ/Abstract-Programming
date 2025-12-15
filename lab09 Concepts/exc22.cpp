3f5fb1f64fa9f7ad274015acce1c2eaf78137891
Piotr <prbartman@gmail.com>
2025-12-15 17:09:29 +0100

    update lab09

--- lab09 Concepts/exc22.cpp
+++ lab09 Concepts/exc22.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <concepts>
+#include <functional>
+
+/***
+ * Zaimplementować koncepty
+ * (i) `Container` który wymaga:
+ *   - atrybutu `value_type`
+ *   - atrybutu `iterator`
+ *   - metody `begin`
+ *   - metody `end`
+ *   - metody `insert(iterator, value_type)`
+ * (ii) `Predicate` który wymaga, aby funkcja przyjmowała typ T,
+ *      a zwracała coś co można interpretować jako bool
+ * (iii) oraz metodę `filterContainer`, która zwraca nowy kontener z elementami
+ *       które spełniają predykat.
+ */
+
 int main() {
     std::vector<int> numbers = {1, 2, 3, 4, 5, 6};
     auto isEven = [](int x) { return x % 2 == 0; };
